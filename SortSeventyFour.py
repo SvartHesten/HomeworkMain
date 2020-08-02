229712d72ef8ec64277d4514b3cfbbdd4ca5cb32
SvartHesten <69052018+SvartHesten@users.noreply.github.com>
2020-08-02 14:01:53 -0700

    Add files via upload

---
+++ SortSeventyFour.py
@@ -0,0 +1,18 @@
+# Sorting three given numbers in ascending order
+
+i = 1
+flist = []
+while i < 4:
+    try:
+        num = int(input("Enter current number"))
+    except ValueError:
+        print(" not a valid number")
+        continue
+    flist.append(num)
+    i += 1
+flist.sort()
+j = 0
+while j < 3:
+    print(f" {flist[j]}  ", end="")
+    j += 1
+
