9eef47c4b92dc12fca3043c8412b142fbdd2e6ed
SvartHesten <69052018+SvartHesten@users.noreply.github.com>
2020-08-06 21:04:21 -0700

    Add files via upload

---
+++ FileFifteen.py
@@ -0,0 +1,28 @@
+# Enter a number
+# find difference between biggest and smallest digits
+
+number_count = 0
+while number_count < 1:
+    number_str = input("Enter our number please")
+    try:
+        number = int(number_str)
+    except ValueError:
+        print(" not a valid number")
+        continue
+    number_count += 1
+    print(f"the number count is equal {number_count}")
+number_list = list(number_str)
+print(number_list)
+maxi = number_list[0]
+for i in range(1, len(number_list)):
+    if number_list[i] >= maxi:
+        maxi = number_list[i]
+print(f" the maximum is {maxi} ")
+mini = number_list[0]
+for i in range(1, len(number_list)):
+    if number_list[i] <= mini:
+        mini = number_list[i]
+print(f" the minimum is  {mini} ")
+maxim = int(maxi)
+minim = int(mini)
+print(f" and the difference is equal to { maxim - minim}")
