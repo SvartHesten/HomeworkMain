de2c27f42ebdc7749b029bb42e20f095c1fc0d64
SvartHesten <69052018+SvartHesten@users.noreply.github.com>
2020-08-07 10:24:46 -0700

    Add files via upload

--- FileFive.py
+++ FileFive.py
@@ -1,20 +1,20 @@
 # moving last digit of integer to front
 # if last digit zero do not move
 
-while True:
+num_count = 0
+while num_count < 1:
     num_str = input(" Enter the number")
     try:
         num = int(num_str)
     except ValueError:
         print(" Ooops , invalid data")
         continue
-    if len(num_str) == 1 or num_str[len(num_str) - 1] == "0":
-        print(f" and the modified number equals {num_str}")
-        break
-    else:
-        first_str = num_str[len(num_str) - 1]
-        for i in range(len(num_str) - 1):
-            first_str = first_str + num_str[i]
-        print(first_str)
-    break
+    num_count += 1
+if len(num_str) == 1 or num_str[len(num_str) - 1] == "0":
+    print(f" and the modified number equals {num_str}")
+else:
+    first_str = num_str[len(num_str) - 1]
+for i in range(len(num_str) - 1):
+    first_str = first_str + num_str[i]
+    print(first_str)
 
