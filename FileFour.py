9a6968bae826bdef0d71247e872ce24a6b4dc173
SvartHesten <69052018+SvartHesten@users.noreply.github.com>
2020-08-07 10:16:27 -0700

    Add files via upload

--- FileFour.py
+++ FileFour.py
@@ -1,19 +1,18 @@
 # input an integer and then calculate the sum
 # n + nn + nnn
 
-while True:
+number_count = 0
+while number_count < 1:
     number_string = input("Enter the positive integer value")
     try:
         number = int(number_string)
-
     except ValueError:
         print(" O o ops, not a valid number")
         continue
-    if number > 0:
-        number1 = number * (10 ** len(number_string)) + number
-        number2 = number * (100 ** len(number_string)) + number *(10 ** len(number_string)) + number
-        print(f" Result equal to: {number + number1 + number2 }")
-        break
-    else:
-        print(" next time enter a positive number")
-        break
+    number_count += 1
+if number > 0:
+    number1 = number * (10 ** len(number_string)) + number
+    number2 = number * (100 ** len(number_string)) + number *(10 ** len(number_string)) + number
+    print(f" Result equal to: {number + number1 + number2 }")
+else:
+    print(" next time enter a positive number")
